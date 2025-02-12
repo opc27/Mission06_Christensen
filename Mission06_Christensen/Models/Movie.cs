34793f8269ecaf8c554cf370e834b5d9da9152f9
Olivia Christensen <oliviapc09@gmail.com>
2025-02-12 14:58:02 -0700

    All done!

--- Mission06_Christensen/Models/Movie.cs
+++ Mission06_Christensen/Models/Movie.cs
@@ -18,7 +18,7 @@ public class Movie
     [Required]
     public string Rating { get; set; }
     public bool Edited { get; set; }
-    public string LentTo { get; set; }
+    public string? LentTo { get; set; }
     [StringLength(25)]
-    public string Notes { get; set; }
+    public string? Notes { get; set; }
 }

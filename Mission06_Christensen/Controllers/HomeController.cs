7aa7861fdf5b9fbe7fc99dbcc84d8ea5ef163b88
Olivia Christensen <oliviapc09@gmail.com>
2025-02-19 14:56:57 -0700

    works

--- Mission06_Christensen/Controllers/HomeController.cs
+++ Mission06_Christensen/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Mission06_Christensen.Models;
 
 namespace Mission06_Christensen.Controllers;
@@ -27,15 +28,80 @@ public class HomeController : Controller
     [HttpGet]
     public IActionResult EnterMovies()
     {
-        return View();
+        ViewBag.Categories = _context.Categories
+            .OrderBy(x => x.CategoryName)
+            .ToList();
+        
+        return View("EnterMovies", new Movie());
     }
     
     [HttpPost]
     public IActionResult EnterMovies(Movie response)
     {
-        _context.Movies.Add(response); // add record to database
+        if (ModelState.IsValid)
+        {
+            _context.Movies.Add(response); // add record to database
+            _context.SaveChanges();
+            
+            return View("Confirmation", response); 
+        }
+        else
+        {
+            ViewBag.Categories = _context.Categories
+                .OrderBy(x => x.CategoryName)
+                .ToList();
+            
+            return View(response);
+        }
+        
+    }
+    
+    public IActionResult ListOfMovies()
+    {
+        // linq
+        var movies = _context.Movies
+            .Include(x => x.Category);
+
+        return View(movies); 
+    }
+    
+    [HttpGet]
+    public IActionResult Edit(int movieid)
+    {
+        var recordToEdit = _context.Movies
+            .Single(x => x.MovieId == movieid);
+        
+        ViewBag.Categories = _context.Categories
+            .OrderBy(x => x.CategoryName)
+            .ToList();  
+        
+        return View("EnterMovies", recordToEdit);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Movie updatedInfo)
+    {
+        _context.Movies.Update(updatedInfo);
         _context.SaveChanges();
         
-        return View("Confirmation", response); 
+        return RedirectToAction("ListOfMovies");
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int movieid)
+    {
+        var recordToDelete = _context.Movies
+            .Single(x => x.MovieId == movieid);
+
+        return View(recordToDelete);
+    }
+
+    [HttpPost]
+    public IActionResult Delete(Movie movie)
+    {
+        _context.Movies.Remove(movie);
+        _context.SaveChanges();
+        
+        return RedirectToAction("ListOfMovies");
     }
 }
--- Mission06_Christensen/Models/Movie.cs
+++ Mission06_Christensen/Models/Movie.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Mission06_Christensen.Models;
 
@@ -6,19 +7,24 @@ public class Movie
 {
     [Key]
     [Required]
-    public int MovieID { get; set; }
-    [Required]
-    public string Category { get; set; }
+    public int MovieId { get; set; }
+    
+    [ForeignKey("CategoryId")]
+    public int? CategoryId { get; set; }
+    public Category Category { get; set; }
+    
     [Required]
     public string Title { get; set; }
     [Required]
+    [Range(1888, int.MaxValue)]
     public int Year { get; set; }
+    public string? Director { get; set; }
+    public string? Rating { get; set; }
     [Required]
-    public string Director { get; set; }
-    [Required]
-    public string Rating { get; set; }
     public bool Edited { get; set; }
     public string? LentTo { get; set; }
+    [Required]
+    public bool CopiedToPlex {get; set;}
     [StringLength(25)]
     public string? Notes { get; set; }
 }
